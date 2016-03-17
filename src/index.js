18ab59130524341cb1205cc0c7b3132676b638d3
Malte Legenhausen <mlegenhausen@gmail.com>
2016-03-17 10:32:59 +0100

    Support for prefixes, suffixes and path transformation added.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 const DEFAULT_SEPARATOR = '_';
+const DEFAULT_PREFIX = '';
+const DEFAULT_SUFFIX = '';
 
-export default function (obj, separator) {
-  return walk(obj, separator);
+export default function (obj, options = {}) {
+  // Allow backwards compatibility with 1.0.0
+  if (isString(options)) {
+    options = { separator: options };
+  }
+
+  return walk(obj, {
+    separator: DEFAULT_SEPARATOR,
+    prefix: DEFAULT_PREFIX,
+    suffix: DEFAULT_SUFFIX,
+    transform: key => key,
+    ...options
+  });
 }
 
-function walk(obj, separator = DEFAULT_SEPARATOR, path = []) {
+function walk(obj, options, path = []) {
   if (!isObject(obj)) {
     throw new Error('pathMirror(...): Argument must be an object.');
   }
+
+  const { prefix, suffix, transform, separator } = options;
 
   const ret = {};
   for (const key in obj) {
     if (obj.hasOwnProperty(key)) {
       const value = obj[key];
+      const currentPath = path.concat(key);
+      const exec = createExec(currentPath);
       if (isObject(value)) {
-        ret[key] = walk(value, separator, path.concat(key));
+        ret[key] = walk(value, options, currentPath);
       } else {
-        ret[key] = path.concat(key).join(separator);
+        ret[key] = exec(prefix) + currentPath.map(transform).join(exec(separator)) + exec(suffix);
       }
     }
   }
@@ -25,4 +42,16 @@ function walk(obj, separator = DEFAULT_SEPARATOR, path = []) {
 
 function isObject(obj) {
   return obj instanceof Object && !Array.isArray(obj);
+}
+
+function isFunction(fn) {
+  return typeof fn === 'function';
+}
+
+function isString(str) {
+  return typeof str === 'string';
+}
+
+function createExec(...args) {
+  return fn => isFunction(fn) ? fn(...args) : fn;
 }
--- test/index.js
+++ test/index.js
@@ -33,25 +33,216 @@ describe('pathMirror', function () {
     });
   });
 
-  it('should use custom separator', function () {
+  xit('should mirror array keys', function () {
     const result = pathMirror({
       blue: {
         'foo': null,
-        'bar': null
+        'bar': [
+          'red',
+          {
+            orange: null
+          },
+          {
+            purple: {
+              foo: null
+            }
+          }
+        ]
       },
       white: null
-    }, '.');
+    });
 
     expect(result).toEqual({
       blue: {
-        foo: 'blue.foo',
-        bar: 'blue.bar'
-      },
-      white: 'white'
+        foo: 'blue_foo',
+        bar: [
+          'blue_bar_0',
+          {
+            orange: 'blue_bar_1_orange'
+          },
+          {
+            purple: {
+              foo: 'blue_bar_2_purple_foo'
+            }
+          }
+        ]
+      }
     });
   });
 
   it('should throw an error on non object', function () {
     expect(pathMirror).toThrow();
+  });
+
+  describe('separator', function () {
+    it('should allow a separator as default option', function () {
+      const result = pathMirror({
+        blue: {
+          'foo': null,
+          'bar': null
+        },
+        white: null
+      }, '.');
+
+      expect(result).toEqual({
+        blue: {
+          foo: 'blue.foo',
+          bar: 'blue.bar'
+        },
+        white: 'white'
+      });
+    });
+
+    it('should allow custom seperator', function () {
+      const result = pathMirror({
+        blue: {
+          'foo': null,
+          'bar': null
+        },
+        white: null
+      }, {
+        separator: '.'
+      });
+
+      expect(result).toEqual({
+        blue: {
+          foo: 'blue.foo',
+          bar: 'blue.bar'
+        },
+        white: 'white'
+      });
+    });
+
+    it('should allow a customer seperator function', function () {
+      const result = pathMirror({
+        blue: {
+          'foo': null,
+          'bar': null
+        },
+        white: {
+          'foo': null,
+          'bar': null
+        }
+      }, {
+        separator: (path) => path[0] === 'blue' ? ':' : '.'
+      });
+
+      expect(result).toEqual({
+        blue: {
+          foo: 'blue:foo',
+          bar: 'blue:bar'
+        },
+        white: {
+          foo: 'white.foo',
+          bar: 'white.bar'
+        }
+      });
+    });
+  });
+
+  describe('prefix', function () {
+    it('should allow a string prefix', function () {
+      const result = pathMirror({
+        blue: {
+          'foo': null,
+          'bar': null
+        },
+        white: null
+      }, {
+        prefix: 'test/'
+      });
+
+      expect(result).toEqual({
+        blue: {
+          foo: 'test/blue_foo',
+          bar: 'test/blue_bar'
+        },
+        white: 'test/white'
+      });
+    });
+
+    it('should allow a prefix function', function () {
+      const result = pathMirror({
+        blue: {
+          'foo': null,
+          'bar': null
+        },
+        white: null
+      }, {
+        prefix: (path) => path[0] === 'blue' ? 'foo/' : 'bar/'
+      });
+
+      expect(result).toEqual({
+        blue: {
+          foo: 'foo/blue_foo',
+          bar: 'foo/blue_bar'
+        },
+        white: 'bar/white'
+      });
+    });
+  });
+
+  describe('suffix', function () {
+    it('should allow a string suffix', function () {
+      const result = pathMirror({
+        blue: {
+          'foo': null,
+          'bar': null
+        },
+        white: null
+      }, {
+        suffix: '/test'
+      });
+
+      expect(result).toEqual({
+        blue: {
+          foo: 'blue_foo/test',
+          bar: 'blue_bar/test'
+        },
+        white: 'white/test'
+      });
+    });
+
+    it('should allow a suffix function', function () {
+      const result = pathMirror({
+        blue: {
+          'foo': null,
+          'bar': null
+        },
+        white: null
+      }, {
+        suffix: (path) => path[0] === 'blue' ? '/1' : '/2'
+      });
+
+      expect(result).toEqual({
+        blue: {
+          foo: 'blue_foo/1',
+          bar: 'blue_bar/1'
+        },
+        white: 'white/2'
+      });
+    });
+  });
+
+  describe('transform', function () {
+    it('should allow the definition of a tranform function', function () {
+      const result = pathMirror({
+        blue: {
+          'foo': null,
+          'bar': null
+        },
+        white: null
+      }, {
+        transform: key => key.toUpperCase()
+      });
+
+      expect(result).toEqual({
+        blue: {
+          foo: 'BLUE_FOO',
+          bar: 'BLUE_BAR'
+        },
+        white: 'WHITE'
+      });
+    });
   });
 });
